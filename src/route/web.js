77d42d4ce00aed5bf2dc64d99f260a331cbd16b2
PhatPhamCoder <phamhoangminhphat@gmail.com>
2022-12-12 13:29:15 +0700

    add db

--- src/route/web.js
+++ src/route/web.js
@@ -6,10 +6,8 @@ let router = express.Router();
 let initWebRouters = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
-
-    // router.get('/hoidanit', (req, res) => {
-    //     return res.send('What do you want')
-    // });
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
 
     return app.use("/", router);
 }
