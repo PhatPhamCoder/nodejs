31d1d6cb20be16016b8b9c1d619a5daf444e96b5
PhatPhamCoder <phamhoangminhphat@gmail.com>
2022-12-12 15:29:41 +0700

    Uydate user and add db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,4 @@
+import { query } from 'express';
 import db from '../models/index';
 import CRUDService from '../services/CRUDService'
 
@@ -29,7 +30,6 @@ let postCRUD = async (req, res) => {
     return res.send('post crud from server');
 }
 
-
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
     console.log('----------------------------')
@@ -40,10 +40,35 @@ let displayGetCRUD = async (req, res) => {
     })
 }
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+    }
+    else {
+        return res.send('User not found');
+    }
+
+}
+
+let getPutCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    getPutCRUD: getPutCRUD,
 }
