aa25032243ecfc4cc508714ac28f9252e34e751c
PhatPhamCoder <phamhoangminhphat@gmail.com>
2022-12-28 11:47:13 +0700

    di cafe 28/12

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -50,18 +50,33 @@ let getAllDoctors = () => {
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown || !inputData.action) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 })
             } else {
-                await db.markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId
-                })
+                if (inputData.action === 'CREATE') {
+                    await db.markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId
+                    })
+                } else if (inputData.action === 'EDIT') {
+                    let doctorMarkdown = await db.markdown.findOne({
+                        where: { doctorId: inputData.doctorId },
+                        raw: false
+                    })
+
+                    if (doctorMarkdown) {
+                        doctorMarkdown = inputData.contentHTML;
+                        doctorMarkdown = inputData.contentMarkdown;
+                        doctorMarkdown = inputData.description;
+                        await doctorMarkdown.save()
+                    }
+
+                }
 
                 resolve({
                     errCode: 0,
