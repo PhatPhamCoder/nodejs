3a06de2e5ae9dc7a15ce5ac80eddfa000200015d
PhatPhamCoder <phamhoangminhphat@gmail.com>
2022-12-31 10:27:36 +0700

    hoan thanh bai 75

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -70,11 +70,27 @@ let bulkCreateSchedule = async (req, res) => {
     }
 }
 
+let getScheduleDoctorByDate = async (req, res) => {
+    try {
+        let infor = await doctorService.getScheduleDoctorByDate(req.query.doctorId, req.query.date);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the sever...'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInforDoctor: postInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
     bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleDoctorByDate: getScheduleDoctorByDate,
 
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,5 @@
 import db from "../models/index";
-import _ from 'lodash';
+import _, { reject } from 'lodash';
 require('dotenv').config();
 
 let getTopDoctorHome = (limitInput) => {
@@ -189,10 +189,40 @@ let bulkCreateSchedule = (data) => {
     })
 }
 
+let getScheduleDoctorByDate = (doctorId, date) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!doctorId || !date) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            } else {
+                let dataSchedule = await db.Schedule.findAll({
+                    where: {
+                        doctorId: doctorId,
+                        date: date
+                    },
+                })
+
+                if (!dataSchedule) dataSchedule = []
+
+                resolve({
+                    errCode: 0,
+                    data: dataSchedule
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
     bulkCreateSchedule: bulkCreateSchedule,
+    getScheduleDoctorByDate: getScheduleDoctorByDate,
 }
